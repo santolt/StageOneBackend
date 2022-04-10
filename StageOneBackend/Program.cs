f96b78fda577bfb0e667c41b4f8054a86339a52d
Santiago Cardona <ssuarez@lean-tech.io>
2022-04-10 18:40:24 -0500

    Add project files.

---
+++ StageOneBackend/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace StageOneBackend
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Opcion1();
+        }
+
+        public static string RemoveSpecialCharacters(string phrase)
+        {
+            char[] arrayPhrase = new char[phrase.Length];
+            int index = 0;
+
+            foreach (char character in phrase)
+            {
+                if ((character >= '0' && character <= '9') || (character >= 'A' && character <= 'Z')
+                    || (character >= 'a' && character <= 'z'))
+                {
+                    arrayPhrase[index] = character;
+                    index++;
+                }
+            }
+            return new string(arrayPhrase, 0, index);
+        }
+
+        public static void Opcion1()
+        {
+            string phrase;
+
+            Console.Write("Write something and I will retorn without special caracters: ");
+            phrase = Console.ReadLine();
+
+            Console.WriteLine(RemoveSpecialCharacters($"Without Special Caracteres: {phrase}"));
+
+        }
+    }
+}
